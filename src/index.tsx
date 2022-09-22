45b7b6b3e6d921c2af2faf5f86c932670dd07171
Ilya Pasternak <PasterZOOM@gmail.com>
2022-09-22 12:30:18 +0300

    Добавил OverlayScrollbars

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
 import { DevSupport } from '@react-buddy/ide-toolbox'
+import OverlayScrollbars from 'overlayscrollbars'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import { App } from 'app/App'
 import { store } from 'app/store'
 import { ComponentPreviews, useInitial } from 'dev'
+import 'overlayscrollbars/css/OverlayScrollbars.css'
 import './index.css'
 
 const container = document.getElementById('root')!
@@ -19,3 +21,6 @@ root.render(
     </DevSupport>
   </Provider>
 )
+const options = { scrollbars: { clickScrolling: true } }
+
+OverlayScrollbars(document.body, options)

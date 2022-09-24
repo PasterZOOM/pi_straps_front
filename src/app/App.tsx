62b04c0e13f8f1dd6af30e9506b89c81d4941bfe
Ilya Pasternak <PasterZOOM@gmail.com>
2022-09-24 22:19:13 +0300

    Заполнил футер

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import { MainPage } from 'common/components/Body/MainPage/MainPage'
+import { Footer } from 'common/components/Footer/Footer'
 import { Header } from 'common/components/Header/Header'
 import { Top } from 'common/components/Top/Top'
 
@@ -10,6 +11,7 @@ export const App = () => {
       <Header />
       <Top />
       <MainPage />
+      <Footer />
     </div>
   )
 }
--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -1,3 +1,22 @@
+import styles from './Footer.module.scss'
+
+import { AppButton } from 'common/components/Buttons/AppButton/AppButton'
+
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <div className={styles.main}>
+      <img
+        src="	https://sun1.beltelecom-by-minsk.userapi.com/s/v1/ig2/dsMeWEeYMzPu-TNVUGl81XFsghiOT2H_VY3_VGO9pUQcXSN-SCyGCdjhNc13jDH42nb0r5uJx2JWpR4l20G_jpOM.jpg?size=400x400&quality=96&crop=71,71,2017,2017&ava=1"
+        alt="logoVK"
+        width="100px"
+        height="100px"
+        className={styles.img}
+      />
+      <div className={styles.title}>PI_Straps | Изделия ручной работы из кожи</div>
+      <a href="https://vk.com/pi_straps" className={styles.a}>
+        На нас подписаны 160 человек
+      </a>
+      <AppButton title="Подробнее" />
+    </div>
+  )
 }

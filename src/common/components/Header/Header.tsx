153696174a0d62cbe68c5e273080f5e8cc6f3b50
Ilya Pasternak <PasterZOOM@gmail.com>
2022-10-02 21:11:33 +0300

    Добавил изменение цвета фона хидера при скролле

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,8 +1,20 @@
+import { useState } from 'react'
+
+import OverlayScrollbars from 'overlayscrollbars'
+
 import styles from './Header.module.scss'
 
 export const Header = () => {
+  const [whiteHeader, setWhiteHeader] = useState(false)
+  const scrollHandler = () => {
+    if (instance.scroll().position.y !== 0) {
+      setWhiteHeader(true)
+    } else setWhiteHeader(false)
+  }
+  const instance = OverlayScrollbars(document.body, { callbacks: { onScroll: scrollHandler } })
+
   return (
-    <div className={styles.main}>
+    <div className={whiteHeader ? `${styles.main} ${styles.whiteHeader}` : styles.main}>
       <div className={styles.start}>
         <img
           src="	https://sun1.beltelecom-by-minsk.userapi.com/s/v1/ig2/dsMeWEeYMzPu-TNVUGl81XFsghiOT2H_VY3_VGO9pUQcXSN-SCyGCdjhNc13jDH42nb0r5uJx2JWpR4l20G_jpOM.jpg?size=400x400&quality=96&crop=71,71,2017,2017&ava=1"
